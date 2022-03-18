0a7d3252e09ae3c35be1d14f4b91879e29c17cb3
kai <iftikharasjad@gmail.com>
2022-03-18 14:29:25 +0500

    D1 pages added!

---
+++ growthmenu/pages/register.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <div className="text-4xl dark:text-white font-thin dark:text-white">
+      Sign Up
+    </div>
+  );
+};
+
+export default Register;
