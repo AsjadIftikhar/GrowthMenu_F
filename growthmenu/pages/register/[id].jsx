f52cf0653625490d90a433a84af130fb740317a3
Asjad Iftikhar <iftkharasjad@gmail.com>
2022-08-03 16:21:24 +0500

    unnecessary files removed, auth update, account left

--- growthmenu/pages/register/[id].jsx
+++ growthmenu/pages/register/[id].jsx
@@ -4,15 +4,9 @@ import Footer from "../../components/footer";
 
 const Profile = () => {
     return (
-        // <div className="bg-LightGrey">
-        //     <div className="bg-LightGrey container mx-auto py-16">
-        //         <Profile_Component/>
-        //     </div>
-        // </div>
         <div className="bg-LightGrey">
             <div className="container mx-auto pt-8 pb-16">
                 <Profile_Component/>
-
             </div>
             <Footer/>
         </div>
